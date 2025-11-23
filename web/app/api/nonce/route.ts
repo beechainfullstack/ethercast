01e413ae1ba6cbc425a4100974e320c193ac7add
beechain <bebrower@gmail.com>
2025-11-22 19:08:20 -0500

    move old code to legacy, full bloatdev js framewoooorker mode

--- web/app/api/nonce/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import crypto from "crypto";
-
-export async function GET() {
-  const nonce = crypto.randomUUID().replace(/-/g, "");
-  return NextResponse.json({ nonce });
-}
